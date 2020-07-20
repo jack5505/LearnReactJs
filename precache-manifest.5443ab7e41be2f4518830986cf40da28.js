a5c170d49cc3749a7cd1b7d37a3bfc10e9abadc2
Jakhongir Sabirov <jahon.080879@gmail.com>
2020-07-20 10:28:19 +0500

    Updates

---
+++ precache-manifest.5443ab7e41be2f4518830986cf40da28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15b8d80cddbc1fae3d6da25a1c689ab1",
+    "url": "/LearnReactJs/index.html"
+  },
+  {
+    "revision": "da133aa1eb34dcdae530",
+    "url": "/LearnReactJs/static/css/main.263358a0.chunk.css"
+  },
+  {
+    "revision": "ca8d57d10fc6c3d1f659",
+    "url": "/LearnReactJs/static/js/2.3aef3f08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LearnReactJs/static/js/2.3aef3f08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da133aa1eb34dcdae530",
+    "url": "/LearnReactJs/static/js/main.72ffb2bb.chunk.js"
+  },
+  {
+    "revision": "567187c39fbaedf741df",
+    "url": "/LearnReactJs/static/js/runtime-main.c7bdb8c0.js"
+  }
+]);
